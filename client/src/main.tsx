f087c3b971c3ecbf44b7c6577effe21ba3c3de5d
Hugo Barbato <hugobarbato@gmail.com>
2026-04-07 19:41:59 -0300

    feat: prepare HTML placeholder and hydrateRoot for SSG

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,26 @@
-import { createRoot } from "react-dom/client";
+import { createRoot, hydrateRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
+import "@fontsource/nunito/400.css";
+import "@fontsource/nunito/500.css";
+import "@fontsource/nunito/600.css";
+import "@fontsource/nunito/700.css";
+import "@fontsource/nunito/800.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
 
-createRoot(document.getElementById("root")!).render(
+const rootEl = document.getElementById("root")!;
+const app = (
   <HelmetProvider>
     <App />
   </HelmetProvider>
 );
+
+// Quando há HTML pré-renderizado, hidrata em vez de criar do zero
+if (rootEl.hasChildNodes()) {
+  hydrateRoot(rootEl, app);
+} else {
+  createRoot(rootEl).render(app);
+}
